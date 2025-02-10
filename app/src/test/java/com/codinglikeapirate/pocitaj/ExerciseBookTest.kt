d2456824cfbf5c081a403327f0ac3926f368501c
Palo Murin <palomurin@gmail.com>
2025-02-10 17:37:07 +0100

    Rewrite ExerciseBookTest in Kotlin

--- app/src/test/java/com/codinglikeapirate/pocitaj/ExerciseBookTest.kt
+++ app/src/test/java/com/codinglikeapirate/pocitaj/ExerciseBookTest.kt
@@ -1,73 +1,76 @@
-package com.codinglikeapirate.pocitaj;
+package com.codinglikeapirate.pocitaj
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Test
 
-public class ExerciseBookTest {
+class ExerciseBookTest {
 
-  @Test
-  public void addition_Question() {
-    ExerciseBook.Exercise exercise = new ExerciseBook.Addition(2, 3);
-    assertEquals("2 + 3", exercise.question());
-  }
+    @Test
+    fun addition_Question() {
+        val exercise = ExerciseBook.Addition(2, 3)
+        assertEquals("2 + 3", exercise.question())
+    }
 
-  @Test
-  public void addition_SolveNotRecognized() {
-    ExerciseBook.Exercise exercise = new ExerciseBook.Addition(2, 3);
-    assertEquals("2 + 3", exercise.question());
-    assertFalse(exercise.solve(ExerciseBook.NOT_RECOGNIZED));
-    assertFalse(exercise.solved());
-    assertFalse(exercise.correct());
-    assertEquals("2 + 3 ≠ ?", exercise.equation());
-  }
+    @Test
+    fun addition_SolveNotRecognized() {
+        val exercise = ExerciseBook.Addition(2, 3)
+        assertEquals("2 + 3", exercise.question())
+        assertFalse(exercise.solve(ExerciseBook.NOT_RECOGNIZED))
+        assertFalse(exercise.solved())
+        assertFalse(exercise.correct())
+        assertEquals("2 + 3 ≠ ?", exercise.equation())
+    }
 
-  @Test
-  public void addition_SolveIncorrectly() {
-    ExerciseBook.Exercise exercise = new ExerciseBook.Addition(4, 2);
-    assertEquals("4 + 2", exercise.question());
-    assertFalse(exercise.solve(7));
-    assertTrue(exercise.solved());
-    assertFalse(exercise.correct());
-    assertEquals("4 + 2 ≠ 7", exercise.equation());
-  }
+    @Test
+    fun addition_SolveIncorrectly() {
+        val exercise = ExerciseBook.Addition(4, 2)
+        assertEquals("4 + 2", exercise.question())
+        assertFalse(exercise.solve(7))
+        assertTrue(exercise.solved())
+        assertFalse(exercise.correct())
+        assertEquals("4 + 2 ≠ 7", exercise.equation())
+    }
 
-  @Test
-  public void addition_SolveCorrectly() {
-    ExerciseBook.Exercise exercise = new ExerciseBook.Addition(2, 3);
-    assertTrue(exercise.solve(5));
-    assertTrue(exercise.solved());
-    assertTrue(exercise.correct());
-    assertEquals("2 + 3 = 5", exercise.equation());
-  }
+    @Test
+    fun addition_SolveCorrectly() {
+        val exercise = ExerciseBook.Addition(2, 3)
+        assertTrue(exercise.solve(5))
+        assertTrue(exercise.solved())
+        assertTrue(exercise.correct())
+        assertEquals("2 + 3 = 5", exercise.equation())
+    }
 
-  @Test
-  public void exerciseBook_generatesQuestionAtStart() {
-    ExerciseBook exerciseBook = new ExerciseBook();
-    assertNotNull(exerciseBook.getLast());
-  }
+    @Test
+    fun exerciseBook_generatesQuestionAtStart() {
+        val exerciseBook = ExerciseBook()
+        assertNotNull(exerciseBook.last)
+    }
 
-  @Test
-  public void exerciseBook_emptyStats() {
-    ExerciseBook exerciseBook = new ExerciseBook();
-    assertEquals("0 / 0 (0%)", exerciseBook.getStats());
-  }
+    @Test
+    fun exerciseBook_emptyStats() {
+        val exerciseBook = ExerciseBook()
+        assertEquals("0 / 0 (0%)", exerciseBook.stats)
+    }
 
-  @Test
-  public void exerciseBook_allWrongStats() {
-    ExerciseBook exerciseBook = new ExerciseBook();
-    int incorrect = 100;
-    exerciseBook.getLast().solve(incorrect);
-    exerciseBook.generate();
-    exerciseBook.getLast().solve(incorrect);
-    assertEquals("0 / 2 (0%)", exerciseBook.getStats());
-  }
+    @Test
+    fun exerciseBook_allWrongStats() {
+        val exerciseBook = ExerciseBook()
+        val incorrect = 100
+        exerciseBook.last.solve(incorrect)
+        exerciseBook.generate()
+        exerciseBook.last.solve(incorrect)
+        assertEquals("0 / 2 (0%)", exerciseBook.stats)
+    }
 
-  @Test
-  public void exerciseBook_oneSolvedOneUnsolvedWrongStats() {
-    ExerciseBook exerciseBook = new ExerciseBook();
-    int incorrect = 100;
-    exerciseBook.getLast().solve(incorrect);
-    exerciseBook.generate();
-    assertEquals("0 / 1 (0%)", exerciseBook.getStats());
-  }
+    @Test
+    fun exerciseBook_oneSolvedOneUnsolvedWrongStats() {
+        val exerciseBook = ExerciseBook()
+        val incorrect = 100
+        exerciseBook.last.solve(incorrect)
+        exerciseBook.generate()
+        assertEquals("0 / 1 (0%)", exerciseBook.stats)
+    }
 }
