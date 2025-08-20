47d7f9f39b90c99e4c2bae93c0e7fee655ce481b
Palo Murin <palomurin@gmail.com>
2025-08-20 18:14:42 +0200

    fix: add missing import

--- app/src/main/java/dev/aidistillery/pocitaj/ui/exercise/ExerciseScreen.kt
+++ app/src/main/java/dev/aidistillery/pocitaj/ui/exercise/ExerciseScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
