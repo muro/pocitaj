1f32937c0c60e3d8f3e90a0dc10897676ebb2fb6
Palo Murin <palomurin@gmail.com>
2025-09-26 17:04:34 +0200

    Migrate the FactMasteryDaoTest to roboelectric, making it run in 500ms.

--- app/src/test/java/dev/aidistillery/pocitaj/data/FactMasteryDaoTest.kt
+++ app/src/test/java/dev/aidistillery/pocitaj/data/FactMasteryDaoTest.kt
@@ -1,18 +1,20 @@
 package dev.aidistillery.pocitaj.data
 
-import android.content.Context
 import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import dev.aidistillery.pocitaj.logic.Curriculum
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
 import java.io.IOException
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE)
 class FactMasteryDaoTest {
     private lateinit var factMasteryDao: FactMasteryDao
     private lateinit var userDao: UserDao
@@ -20,10 +22,10 @@ class FactMasteryDaoTest {
 
     @Before
     fun createDb() {
-        val context = ApplicationProvider.getApplicationContext<Context>()
+        val context = RuntimeEnvironment.getApplication()
         db = Room.inMemoryDatabaseBuilder(
             context, AppDatabase::class.java
-        ).build()
+        ).allowMainThreadQueries().build()
         factMasteryDao = db.factMasteryDao()
         userDao = db.userDao()
     }
@@ -39,21 +41,23 @@ class FactMasteryDaoTest {
     fun upsertAndGetFactMasteryMastery() = runBlocking {
         val user = User(name = "test_user")
         val userId = userDao.insert(user)
+        val level = Curriculum.SubtractingTens.id
 
         val factMastery = FactMastery(
             factId = "ADDITION_5_3",
             userId = userId,
-            level = "",
+            level = level,
             strength = 3,
             lastTestedTimestamp = System.currentTimeMillis()
         )
         factMasteryDao.upsert(factMastery)
-        val retrievedFactMastery = factMasteryDao.getFactMastery(userId, "ADDITION_5_3")
+        val retrievedFactMastery = factMasteryDao.getFactMastery(userId, "ADDITION_5_3", level)
         assertEquals(factMastery.strength, retrievedFactMastery?.strength)
 
         val updatedFactMastery = factMastery.copy(strength = 4)
         factMasteryDao.upsert(updatedFactMastery)
-        val updatedRetrievedFactMastery = factMasteryDao.getFactMastery(userId, "ADDITION_5_3")
+        val updatedRetrievedFactMastery =
+            factMasteryDao.getFactMastery(userId, "ADDITION_5_3", level)
         assertEquals(updatedFactMastery.strength, updatedRetrievedFactMastery?.strength)
     }
 }
